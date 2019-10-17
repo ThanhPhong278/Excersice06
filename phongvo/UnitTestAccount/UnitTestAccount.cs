6949882c6b6e20f44b9d949a61b5010fce835e36
thanhphong <phongvo2728@gmail.com>
2019-10-17 10:56:25 +0700

    bankAccount Excer06

---
+++ phongvo/UnitTestAccount/UnitTestAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using phongvo;
+namespace UnitTestAccount
+{
+    [TestClass]
+    public class UnitTestAccount
+    {
+        [TestMethod]
+        public void TestMethodAccount()
+        {
+            Account acc = new Account(200000);
+
+            Assert.IsNotNull(acc);
+            Assert.IsInstanceOfType(acc, typeof(Account));
+            Assert.AreEqual(200000, acc.Balance());
+
+        }
+
+        public void TestMethodWithDraw()
+        {
+            Account acc = new Account(500000);
+            acc.Withdraw(200000);
+            Assert.AreEqual(700000, acc.Balance());
+            acc.Withdraw(300000);
+            Assert.AreEqual(1000000, acc.Balance());
+        }
+
+        public void TestMethodDeposit()
+        {
+            Account acc = new Account(500000);
+            acc.Deposit(200000);
+            Assert.AreEqual(300000, acc.Balance());
+            acc.Deposit(300000);
+            Assert.AreEqual(0, acc.Balance());
+        }
+
+        public void TestMethodBalance()
+        {
+            Account acc = new Account(500000);
+            acc.Deposit(200000);
+            Assert.AreEqual(300000, acc.Balance());
+            acc.Withdraw(300000);
+            Assert.AreEqual(800000, acc.Balance());
+        }
+    }
+}
---
+++ phongvo/phongvo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phongvo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
